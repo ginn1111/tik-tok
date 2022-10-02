4c424596011132fb24458d6a1e9384a33c6e230e
ginn1111 <vanthuanjw@gmail.com>
2022-10-02 22:24:29 +0700

    feat: generalize defautl layout

--- src/Layout/DefaultLayout/index.tsx
+++ src/Layout/DefaultLayout/index.tsx
@@ -1,16 +1,19 @@
 import Header from '../components/Header';
 import Sidebar from './Sidebar';
 import type { LayoutProps } from '../../models';
+import styles from './DefaultLayout.module.scss';
 
 const DefaultLayout = ({ children }: LayoutProps) => {
   return (
-    <>
+    <div className={styles.wrapper}>
       <Header />
-      <div>
-        <Sidebar />
-        <main>{children}</main>
+      <div className={styles.container}>
+        <aside className={styles.sideBar}>
+          <Sidebar />
+        </aside>
+        <main className={styles.content}>{children}</main>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/Layout/components/Header/index.tsx
+++ src/Layout/components/Header/index.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import styles from './Header.module.scss';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={styles.wrapper}>
+      <div className={styles.container}></div>
+    </header>
+  );
 };
 
 export default Header;
